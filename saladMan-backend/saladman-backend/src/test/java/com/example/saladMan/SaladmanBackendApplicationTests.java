175184fa3556187ab1fdd882fc6a71e44abc0d2b
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-06-09 04:46:40 +0900

    [create] 백엔드 프로젝트 생성

---
+++ saladMan-backend/saladman-backend/src/test/java/com/example/saladMan/SaladmanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saladMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladmanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
