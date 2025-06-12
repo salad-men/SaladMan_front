a558bfa76d9e2d2610e3bb2cfb6db88a35eaf6bb
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-06-12 10:00:46 +0900

    [test] CI/CD테스트

--- saladMan-frontend/src/component/config.js
+++ saladMan-frontend/src/component/config.js
@@ -1,2 +1,6 @@
+// 예: 환경변수 REACT_APP_API_ENV가 'prod'이면 S3 주소, 아니면 localhost
 
-export const url = "http://localhost:8080"; 
+const localUrl = "http://localhost:5173";
+const prodUrl = "http://saladman-web.s3-website.ap-northeast-2.amazonaws.com";
+
+export const url = process.env.REACT_APP_API_ENV === "prod" ? prodUrl : localUrl;
--- saladMan-frontend/src/config.js
+++ saladMan-frontend/src/config.js
@@ -1,1 +1,5 @@
-export const url = "http://43.201.50.155:8090"
+console.log("현재 Vite 모드:", import.meta.env.MODE);
+
+export const url = import.meta.env.VITE_API_ENV === "prod"
+  ? import.meta.env.VITE_API_URL
+  : "http://localhost:5173";
