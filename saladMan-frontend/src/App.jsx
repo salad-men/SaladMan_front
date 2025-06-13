2fedb6ff36db82287e8112134357cf76c86cdf43
jeun10 <luluka11@naver.com>
2025-06-13 18:24:04 +0900

    [Docs] App.jsx 파일 구조 변경

--- saladMan-frontend/src/App.jsx
+++ saladMan-frontend/src/App.jsx
@@ -75,96 +75,109 @@ function App() {
 
   return (
     <>
-      {/* <Header/> */}
-
       <Routes>
-        <Route path='/hq/HqInventoryList' element={<HqInventoryList />} />
-        <Route path='/hq/HqInventoryExpiration' element={<HqInventoryExpiration />} />
-        <Route path='/hq/HqDisposalList' element={<HqDisposalList />} />
-        <Route path='/hq/HqIngredientSetting' element={<HqIngredientSetting />} />
-        <Route path='/hq/HqInventoryRecord' element={<HqInventoryRecord />} />
-
-        <Route path='/store/StoreInventoryList' element={<StoreInventoryList />} />
-        <Route path='/store/StoreInventoryExpiration' element={<StoreInventoryExpiration />} />
-        <Route path='/store/StoreDisposalList' element={<StoreDisposalList />} />
-        <Route path='/store/StoreIngredientSetting' element={<StoreIngredientSetting />} />
-        <Route path='/store/StoreInventoryRecord' element={<StoreInventoryRecord />} />
-
-        <Route path='/hq/HqNoticeList' element={<HqNoticeList />} />
-        <Route path='/hq/HqNoticeDetail' element={<HqNoticeDetail />} />
-
-        <Route path='/store/StoreNoticeList' element={<StoreNoticeList />} />
-        <Route path='/store/StoreNoticeDetail' element={<StoreNoticeDetail />} />
-
-        <Route path='/hq/HqComplaintList' element={<HqComplaintList />} />
-        <Route path='/hq/HqComplaintDetail' element={<HqComplaintDetail />} />
-
-        <Route path='/store/StoreComplaintList' element={<StoreComplaintList />} />
-        <Route path='/store/StoreComplaintDetail' element={<StoreComplaintDetail />} /> 
-
-        <Route path="/mainPage" element={<MainPage />} />
-
-        <Route path="/brandIntro" element={<BrandIntro />} />
-        <Route path="/sloganIntro" element={<SloganIntro />} />
-        <Route path="/menuPage" element={<MenuPage />} />
-        <Route path="/nutritionPage" element={<Nutrition />} />
-        <Route path="/findStore" element={<FindStorePage />} />
-
-        <Route path="/MainPage" element={<MainPage />} />
-        <Route path="/BrandIntro" element={<BrandIntro />} />
-        <Route path="/SloganIntro" element={<SloganIntro />} />
-        <Route path="/MenuPage" element={<MenuPage />} />
-        <Route path="/NutritionPage" element={<Nutrition />} />
-        <Route path="/News" element={<NewsPage />} />
-        <Route path="/NewsDetail" element={<NewsDetailPage />} />
-        <Route path="/PraiseStore" element={<PraiseStorePage />} />
-        <Route path="/Event" element={<EventPage />} />
-        <Route path="/FindStore" element={<FindStorePage />} />
-        <Route path="/Kiosk" element={<KioskPage />} />
-        <Route path="/KioskMenu" element={<KioskMenuPage />} />
-
-        {/*본사 매장관리 */}
-        <Route path="/hq/storeRegister" element={<StoreRegister />} />
-        <Route path="/hq/storeAccount" element={<StoreAccountList />} />
-        <Route path="/hq/empRegister" element={<EmployeeRegister />} />
-        <Route path="/hq/empList" element={<EmployeeList />} />
-        {/*매장 매장관리*/}
-        <Route path="/store/empSchedule" element={<EmpSchedule />} />
-
-        {/*본사 발주*/}
-        <Route path="/hq/orderRequest" element={<OrderRequestList />} />
-        <Route path="/hq/orderRequestDetail" element={<OrderRequestDetail />} />
-        <Route path="/hq/orderItemManage" element={<OrderItemManage />} />
-        {/*매장 발주*/}
-        <Route path="/store/orderList" element={<OrderList />} />
-        <Route path="/store/orderApply" element={<OrderApply />} />
-        <Route path="/store/orderDetail" element={<OrderDetail />} />
-        <Route path="/store/stockInspection" element={<StockInspection />} />
-        <Route path="/store/orderSettings" element={<OrderSettings />} />
-        <Route path="/store/stockLog" element={<StockLog />} />
 
         <Route element={<HqLayout />}>
+
+          {/* 재고 */}
+          <Route path='/hq/HqInventoryList' element={<HqInventoryList />} />
+          <Route path='/hq/HqInventoryExpiration' element={<HqInventoryExpiration />} />
+          <Route path='/hq/HqDisposalList' element={<HqDisposalList />} />
+          <Route path='/hq/HqIngredientSetting' element={<HqIngredientSetting />} />
+          <Route path='/hq/HqInventoryRecord' element={<HqInventoryRecord />} />
+
+          {/*발주*/}
+          <Route path="/hq/orderRequest" element={<OrderRequestList />} />
+          <Route path="/hq/orderRequestDetail" element={<OrderRequestDetail />} />
+          <Route path="/hq/orderItemManage" element={<OrderItemManage />} />
+
           {/* 메뉴 */}
           <Route path='/hq/allMenus' element={<HqAllMenus />} />
           <Route path='/hq/updateMenu' element={<HqUpdateMenu />} />
           <Route path='/hq/recipe' element={<HqRecipe />} />
+
           {/* 매출 */}
           <Route path='/hq/totalSales' element={<HqTotalSales />} />
           <Route path='/hq/storeSales' element={<HqStoreSales />} />
+
+          {/*매장관리 */}
+          <Route path="/hq/storeRegister" element={<StoreRegister />} />
+          <Route path="/hq/storeAccount" element={<StoreAccountList />} />
+          <Route path="/hq/empRegister" element={<EmployeeRegister />} />
+          <Route path="/hq/empList" element={<EmployeeList />} />
+
+          {/* 점포조회 */}
           <Route path='/hq/storeStock' element={<HqStoreStock />} />
-          {/* 공지사항 */}
+
+          {/* 공지- 공지 */}
+          <Route path='/hq/HqNoticeList' element={<HqNoticeList />} />
+          <Route path='/hq/HqNoticeDetail' element={<HqNoticeDetail />} />
+          {/* 공지-불만 */}
+          <Route path='/hq/HqComplaintList' element={<HqComplaintList />} />
+          <Route path='/hq/HqComplaintDetail' element={<HqComplaintDetail />} />
+          {/* 공지-알림목록 */}
           <Route path='/hq/notification' element={<HqNotification />} />
-        </Route>
-        <Route element={<StoreLayout/>}>
-          <Route path='/store/allMenus' element={<AllMenus/>}/>
-          <Route path='/store/menuStatus' element={<MenuStatus/>}/>
-          <Route path='/store/recipe' element={<Recipe/>}/>
-          <Route path='/store/findOtherStore' element={<FindOtherStore/>}/>
-          <Route path='/store/otherStoreInven' element={<OtherStoreInven/>}/>
-          <Route path='/store/notification' element={<Notification/>}/>
+
         </Route>
 
+        <Route element={<StoreLayout />}>
+
+          {/* 재고 */}
+          <Route path='/store/StoreInventoryList' element={<StoreInventoryList />} />
+          <Route path='/store/StoreInventoryExpiration' element={<StoreInventoryExpiration />} />
+          <Route path='/store/StoreDisposalList' element={<StoreDisposalList />} />
+          <Route path='/store/StoreIngredientSetting' element={<StoreIngredientSetting />} />
+          <Route path='/store/StoreInventoryRecord' element={<StoreInventoryRecord />} />
+          
+          {/*매장 발주*/}
+          <Route path="/store/orderList" element={<OrderList />} />
+          <Route path="/store/orderApply" element={<OrderApply />} />
+          <Route path="/store/orderDetail" element={<OrderDetail />} />
+          <Route path="/store/stockInspection" element={<StockInspection />} />
+          <Route path="/store/orderSettings" element={<OrderSettings />} />
+          <Route path="/store/stockLog" element={<StockLog />} />
+          
+          {/* 메뉴 */}
+          <Route path='/store/allMenus' element={<AllMenus />} />
+          <Route path='/store/menuStatus' element={<MenuStatus />} />
+          <Route path='/store/recipe' element={<Recipe />} />
+
+          {/*매장관리*/}
+          <Route path="/store/empSchedule" element={<EmpSchedule />} />
+          
+          {/* 매출 */}
+
+          {/* 점포조회 */}
+          <Route path='/store/findOtherStore' element={<FindOtherStore />} />
+          <Route path='/store/otherStoreInven' element={<OtherStoreInven />} />
+
+          {/* 공지사항 */}
+          <Route path='/store/StoreNoticeList' element={<StoreNoticeList />} />
+          <Route path='/store/StoreNoticeDetail' element={<StoreNoticeDetail />} />
+          <Route path='/store/notification' element={<Notification />} />
+
+          {/* 공지 -고객불만사항 */}
+          <Route path='/store/StoreComplaintList' element={<StoreComplaintList />} />
+          <Route path='/store/StoreComplaintDetail' element={<StoreComplaintDetail />} />
+
+        </Route>
+
+        {/* 로그인페이지 */}
         <Route path='/' element={<Login />} />
+
+        {/* 소비자 메인 페이지 */}
+        <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/brandIntro" element={<BrandIntro />} />
+        <Route path="/sloganIntro" element={<SloganIntro />} />
+        <Route path="/menuPage" element={<MenuPage />} />
+        <Route path="/nutritionPage" element={<Nutrition />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/newsDetail" element={<NewsDetailPage />} />
+        <Route path="/praiseStore" element={<PraiseStorePage />} />
+        <Route path="/event" element={<EventPage />} />
+        <Route path="/findStore" element={<FindStorePage />} />
+        <Route path="/kiosk" element={<KioskPage />} />
+        <Route path="/kioskMenu" element={<KioskMenuPage />} />
 
       </Routes>
     </>
