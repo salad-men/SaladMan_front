fdf64b9dd3a90d1a04f375bd316b2fabffc501e8
jeun10 <luluka11@naver.com>
2025-06-16 17:11:53 +0900

    [Fetch] 본사 매장 등록 기능 구현

--- saladMan-frontend/src/component/hq/storeManagement/StoreRegister.jsx
+++ saladMan-frontend/src/component/hq/storeManagement/StoreRegister.jsx
@@ -5,31 +5,51 @@ import DaumPostcode from "react-daum-postcode";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { Modal, Button } from "antd";
 import { useNavigate } from "react-router";
+import { myAxios } from "/src/config.jsx";
 
 export default function StoreRegister() {
 
     const mapContainerRef = useRef();
     const [isOpen, setIsOpen] = useState(false);
-    const [store, setStore] = useState({ storeName: '', address: '', detailAddress: '', phoneNumber: '', storeAccount: '', storePassword: '' });
+    const [store, setStore] = useState({ storeName: '', address: '', detailAddress: '', phoneNumber: '', storeAccount: '', storePassword: '', region: '' });
     const [coords, setCoords] = useState({ lat: 37.5665, lng: 126.9780 }); // 기본값: 서울시청
+
+    const [storeNameChecked, setStoreNameChecked] = useState(false);
+    const [usernameChecked, setUsernameChecked] = useState(false);
+
+    const [isStoreNameValid, setIsStoreNameValid] = useState(null); // true, false, null
+    const [isUsernameValid, setIsUsernameValid] = useState(null);
+
     const [isMapReady, setIsMapReady] = useState(false);
 
+    const simplifySidoToRegion = (sido) => {
+        const map = {
+            "서울특별시": "서울",
+            "부산광역시": "부산",
+            "대구광역시": "대구",
+            "인천광역시": "인천",
+            "광주광역시": "광주",
+            "대전광역시": "대전",
+            "울산광역시": "울산",
+            "세종특별자치시": "세종",
+            "경기도": "경기",
+            "강원특별자치도": "강원",
+            "충청북도": "충북",
+            "충청남도": "충남",
+            "전라북도": "전북",
+            "전라남도": "전남",
+            "경상북도": "경북",
+            "경상남도": "경남",
+            "제주특별자치도": "제주"
+        };
+
+        return map[sido] || sido;
+    }
 
     const edit = (e) => {
         setStore({ ...store, [e.target.name]: e.target.value });
     }
     const navigate = useNavigate();
-    const themeObj = {
-        bgColor: "", 			// 바탕 배경색
-        searchBgColor: "", 		// 검색창 배경색
-        contentBgColor: "", 		// 본문 배경색(검색결과,결과없음,첫화면,검색서제스트)
-        pageBgColor: "", 		// 페이지 배경색
-        textColor: "", 			// 기본 글자색
-        queryTextColor: "", 		// 검색창 글자색
-        postcodeTextColor: "", 	// 우편번호 글자색
-        emphTextColor: "", 		// 강조 글자색
-        outlineColor: "" 		// 테두리
-    };
 
     const postCodeStyle = {
         width: '360px',
@@ -52,10 +72,21 @@ export default function StoreRegister() {
         marker.setMap(map);
     }, [coords]);
 
+    useEffect(() => {
+        setStoreNameChecked(false);
+        setIsStoreNameValid(null);
+    }, [store.storeName]);
+
+    useEffect(() => {
+        setUsernameChecked(false);
+        setIsUsernameValid(null);
+
+    }, [store.storeAccount]);
 
     const completeHandler = (data) => {
-        const { address } = data;
-        setStore({ ...store, address: address });
+        const { address, sido } = data;
+        const region = simplifySidoToRegion(sido);
+        setStore({ ...store, address: address, region: region });
         setIsOpen(false);
 
         // 주소 → 좌표 변환
@@ -80,56 +111,165 @@ export default function StoreRegister() {
         }
     };
 
+    const checkStoreName = async () => {
+        if (!store.storeName) {
+            alert("매장이름을 입력해주세요.");
+            return;
+        }
+
+        try {
+            const axiosInstance = myAxios(null);
+            const res = await axiosInstance.get(`/hq/checkStorename`, {
+                params: { name: store.storeName }
+            });
+            setIsStoreNameValid(!res.data);
+
+            if (res.data) {
+                alert("이미 사용 중인 매장이름입니다.");
+                setStoreNameChecked(false);
+            }
+            else {
+                alert("사용 가능한 매장이름입니다.");
+                setStoreNameChecked(true);
+
+            }
+
+        } catch (err) {
+            console.error(err);
+            alert("매장이름 중복 확인 중 오류");
+            setStoreNameChecked(false);
+
+        }
+    };
+
+    const checkUsername = async () => {
+        if (!store.storeAccount) {
+            alert("아이디를 입력해주세요.");
+            return;
+        }
+
+        try {
+            const axiosInstance = myAxios(null);
+            const res = await axiosInstance.get(`/hq/checkUsername`, {
+                params: { username: store.storeAccount }
+            });
+            setIsUsernameValid(!res.data);
+            if (res.data) {
+                alert("이미 사용 중인 아이디입니다.");
+                setUsernameChecked(false);
+            }
+            else {
+                alert("사용 가능한 아이디입니다.");
+                setUsernameChecked(true);
+
+            }
+
+        } catch (err) {
+            console.error(err);
+            alert("아이디 중복 확인 중 오류");
+            setUsernameChecked(false);
+
+        }
+    };
+
+    const registerStore = async (e) => {
+
+        e.preventDefault();
+
+        if (!storeNameChecked) {
+            alert("매장이름 중복 확인을 해주세요.");
+            return;
+        }
+
+        if (!usernameChecked) {
+            alert("아이디 중복 확인을 해주세요.");
+            return;
+        }
+        const payload = {
+            name: store.storeName,
+            address: store.address + ' ' + store.detailAddress,
+            phoneNumber: store.phoneNumber,
+            username: store.storeAccount,
+            password: store.storePassword,
+            location: store.region,
+            latitude: coords.lat,
+            longitude: coords.lng
+        };
+        try {
+            const axiosInstance = myAxios(null); // 토큰 없으면 null
+            const response = await axiosInstance.post("/hq/storeRegister", payload);
+
+            if (response.data === true) {
+                alert("매장 등록 완료");
+                navigate("/hq/storeAccount");
+            } else {
+                alert("등록 실패");
+            }
+        } catch (error) {
+            console.error("등록 오류:", error);
+            alert("서버 오류 발생");
+        }
+    }
+
 
     return (
         <>
 
 
-                <div className="storeRegisterContainer">
+            <div className="storeRegisterContainer">
                 <EmpSidebar />
-                    <div className="mainContent">
-                        <h2>매장 등록</h2>
-                        <div className="formSection">
-                            <table className="registerTable">
-                                <tbody>
-                                    <tr>
-                                        <td>매장이름</td>
-                                        <td><input type="text" name="storeName" onChange={edit} /></td>
-                                    </tr>
-                                    <tr>
-                                        <td>매장위치</td>
-                                        <td><input type="button" value="주소 찾기" onClick={() => setIsOpen(!isOpen)} /><br />
-                                            <input type="text" id="address" name="address" placeholder="주소" value={store.address} onChange={edit} /><br />
-                                            <input type="text" id="detailAddress" name="detailAddress" placeholder="상세주소" onChange={edit} />
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>매장 전화번호</td>
-                                        <td><input type="text" name="phoneNumber" placeholder="예: 02-123-4567" onChange={edit} /></td>
-                                    </tr>
-                                    <tr>
-                                        <td>매장 계정</td>
-                                        <td>
-                                            <input type="text" name="storeAccount" placeholder="아이디" onChange={edit} />
-                                            <input type="password" name="storePassword" placeholder="비밀번호" onChange={edit} />
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <div className="mapBox">
-                                <div
-                                    ref={mapContainerRef}
-                                    style={{ width: '500px', height: '400px' }}
-                                ></div>
-                            </div>
-
+                <div className="mainContent">
+                    <h2>매장 등록</h2>
+                    <div className="formSection">
+                        <table className="registerTable">
+                            <tbody>
+                                <tr>
+                                    <td>매장이름</td>
+                                    <td>
+                                        <input type="text" name="storeName" onChange={edit} className={
+                                            isStoreNameValid === false ? "inputFalse"
+                                                : isStoreNameValid === true ? "inputSuccess"
+                                                    : ""}
+                                        />
+                                        <button type="button" className="checkButton" onClick={checkStoreName}>중복확인</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>매장위치</td>
+                                    <td>
+                                        <input type="text" id="address" name="address" placeholder="주소" value={store.address} onChange={edit} />
+                                        <input type="button" value="검색" onClick={() => setIsOpen(!isOpen)} /><br />
+                                        <input type="text" id="detailAddress" name="detailAddress" placeholder="상세주소" onChange={edit} />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>매장 전화번호</td>
+                                    <td><input type="text" name="phoneNumber" placeholder="예: 02-123-4567" onChange={edit} /></td>
+                                </tr>
+                                <tr>
+                                    <td>매장 계정</td>
+                                    <td>
+                                        <input type="text" name="storeAccount" placeholder="아이디" onChange={edit} className={isUsernameValid === false ? "inputFalse" : isUsernameValid === true ? "inputSuccess" : ""} />
+                                        <button type="button" className="checkButton" onClick={checkUsername}>중복확인</button>
+                                        <input type="password" name="storePassword" placeholder="비밀번호" onChange={edit} />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className="mapBox">
+                            <div
+                                ref={mapContainerRef}
+                                style={{ width: '500px', height: '400px' }}
+                            ></div>
                         </div>
 
-                        <div className="submitSection">
-                            <button className="submitButton">저장</button>
-                        </div>
+                    </div>
+
+                    <div className="submitSection">
+                        <button className="submitButton" onClick={registerStore}>저장</button>
                     </div>
                 </div>
+            </div>
             {isOpen &&
                 <Modal title="주소입력"
                     open={isOpen} footer={null} >
--- saladMan-frontend/src/config.jsx
+++ saladMan-frontend/src/config.jsx
@@ -6,16 +6,16 @@ export const url = import.meta.env.VITE_API_URL || "http://localhost:8090";
 
 export const myAxios = (token) => {
    var instance = axios.create({
-      baseURL : url,
-      timeout:5000,
+      baseURL: url,
+      timeout: 5000,
    })
 
-   token && instance.interceptors.request.use((config)=>{
-    config.headers.Authorization = token;
-    return config;
-  });
+   token && instance.interceptors.request.use((config) => {
+      config.headers.Authorization = token;
+      return config;
+   });
 
-  return instance;
+   return instance;
 }
 
 
