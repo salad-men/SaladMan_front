e141e5e19841e4ab2e54bc6dcf7aac3bd1981ec2
jhzzng <jhzzng01@gmail.com>
2025-06-18 17:19:43 +0900

    [create] chatbot 제작중

--- saladMan-frontend/src/component/user/page/MainPage.jsx
+++ saladMan-frontend/src/component/user/page/MainPage.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import Layout from '../mainPage/Layout';
 import Header from '../common/Header';
 import Herosection from '../mainPage/Herosection';
@@ -8,12 +6,18 @@ import IntroSection2 from '../mainPage/IntroSection2';
 import SearchStore from '../mainPage/SearchStore';
 import BestMenuSection from '../mainPage/BestMenuSection';
 import Footer from '../common/Footer';
+import Chatbot from '@user/common/Chatbot';
+
+import React, { useState } from "react";
 
 const MainPage = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
     <Header />
     <Herosection />
+      <Chatbot isOpen={isOpen} setIsOpen={setIsOpen} />
       <Layout>
       <main>
         <IntroSection1 />
