fa6c24f20a1a10d47c26713ad64c63fb69189703
jeun10 <luluka11@naver.com>
2025-07-04 09:16:59 +0900

    [Docs] 네트워크 설정

--- saladMan-frontend/src/config.jsx
+++ saladMan-frontend/src/config.jsx
@@ -7,7 +7,7 @@ console.log("현재 Vite 모드:", import.meta.env.MODE);
 const store = getDefaultStore();
 
 export const API_BASE = import.meta.env.VITE_API_URL;
-export const CF_BASE  = import.meta.env.VITE_CLOUDFRONT_URL || '';
+export const CF_BASE = import.meta.env.VITE_CLOUDFRONT_URL || '';
 
 export const myAxios = (token) => {
    var instance = axios.create({
@@ -39,7 +39,7 @@ export const myAxios = (token) => {
       }
    );
 
-    return instance;
+   return instance;
 }
 
 export const themeObj = {
@@ -53,3 +53,53 @@ export const themeObj = {
    emphTextColor: "", 		// 강조 글자색
    outlineColor: "" 		// 테두리
 };
+
+// export const myAxios = (token) => {
+//    let baseURL;
+
+//    // 현재 브라우저 hostname 가져오기
+//    const hostname = window.location.hostname;
+
+//    if (hostname === "localhost") {
+//       // 로컬 개발환경
+//       baseURL = "http://localhost:8090";
+//    } else {
+//       // 같은 네트워크(아이패드, 다른 PC)
+//       baseURL = "http://192.168.0.9:8090";
+//    }
+
+//    console.log("현재 baseURL:", baseURL);
+
+//    const instance = axios.create({
+//       baseURL: baseURL,
+//       timeout: 5000,
+//       withCredentials: true, // 세션/쿠키 필요하면 유지
+//    });
+
+//    // 요청 인터셉터
+//    if (token) {
+//       instance.interceptors.request.use((config) => {
+//          config.headers.Authorization = token;
+//          return config;
+//       });
+//    }
+
+//    // 응답 인터셉터
+//    instance.interceptors.response.use(
+//       (response) => response,
+//       (error) => {
+//          const code = error.response?.status;
+
+//          if (code === 401 || code === 403) {
+//             alert("접근 권한이 없습니다.");
+//             store.set(accessTokenAtom, '');
+//             store.set(refreshTokenAtom, '');
+//             window.location.href = "/";
+//          }
+
+//          return Promise.reject(error);
+//       }
+//    );
+
+//    return instance;
+// };
