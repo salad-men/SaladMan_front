b48ef06ebe96134d2a4dea62d9fe75260cfeca45
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-07-07 21:14:18 +0900

    [design] 본사 메뉴 화면 수정

--- saladMan-frontend/src/component/hq/Menu/HqSidebarMenu.jsx
+++ saladMan-frontend/src/component/hq/Menu/HqSidebarMenu.jsx
@@ -1,16 +1,20 @@
-import'../HqSidebar.css'
+import styles from '../Inventory/HqInventorySidebar.module.css'; 
 
-export default function HqSidebarMenus() {
-    return(
-        <>
-            <div className="sidebar">
-                <h1>메뉴</h1>
-                <ul>
-                    <li><a href="/hq/totalMenu">전체 메뉴</a></li>
-                    <li><a href="/hq/updateMenu">메뉴 등록</a></li>
-                    <li><a href="/hq/recipe">레시피 조회</a></li>
-                </ul>
-            </div>
-        </>
-    )
+export default function HqSidebarMenu() {
+    return (
+        <div className={styles.sidebar}>
+            <h1 className={styles.title}>메뉴</h1>
+            <ul className={styles.list}>
+                <li className={styles.listItem}>
+                    <a href="/hq/totalMenu" className={styles.link}>전체 메뉴</a>
+                </li>
+                <li className={styles.listItem}>
+                    <a href="/hq/updateMenu" className={styles.link}>메뉴 등록</a>
+                </li>
+                <li className={styles.listItem}>
+                    <a href="/hq/recipe" className={styles.link}>레시피 조회</a>
+                </li>
+            </ul>
+        </div>
+    );
 }
