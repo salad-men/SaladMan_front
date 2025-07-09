dd4f5ba70bfc835eeb55af7864dab4a5c197317c
jeun10 <luluka11@naver.com>
2025-07-09 17:35:34 +0900

    [Docs] config 수정

--- saladMan-frontend/src/config.jsx
+++ saladMan-frontend/src/config.jsx
@@ -9,91 +9,29 @@ const store = getDefaultStore();
 export const API_BASE = import.meta.env.VITE_API_URL;
 export const CF_BASE = import.meta.env.VITE_CLOUDFRONT_URL || '';
 
-// export const myAxios = (token) => {
-//    var instance = axios.create({
-//       baseURL: API_BASE,
-//       timeout: 5000,
-//    })
-
-//    token && instance.interceptors.request.use((config) => {
-
-//       config.headers.Authorization = token;
-//       return config;
-//    });
-
-//    instance.interceptors.response.use(
-//       response => response,
-//       error => {
-//          const code = error.response?.status;
-
-//          if (code === 401 || code === 403) {
-//             alert("접근 권한이 없습니다.");
-
-//             store.set(accessTokenAtom, '');
-//             store.set(refreshTokenAtom, '');
-
-//             window.location.href = "/";
-//          }
-
-//          return Promise.reject(error);
-//       }
-//    );
-
-//    return instance;
-// }
-
-// export const themeObj = {
-//    bgColor: "", 			// 바탕 배경색
-//    searchBgColor: "", 		// 검색창 배경색
-//    contentBgColor: "", 		// 본문 배경색(검색결과,결과없음,첫화면,검색서제스트)
-//    pageBgColor: "", 		// 페이지 배경색
-//    textColor: "", 			// 기본 글자색
-//    queryTextColor: "", 		// 검색창 글자색
-//    postcodeTextColor: "", 	// 우편번호 글자색
-//    emphTextColor: "", 		// 강조 글자색
-//    outlineColor: "" 		// 테두리
-// };
-
 export const myAxios = (token) => {
-   let baseURL;
-
-   // 현재 브라우저 hostname 가져오기
-   const hostname = window.location.hostname;
-
-   if (hostname === "localhost") {
-      // 로컬 개발환경
-      baseURL = "http://localhost:8090";
-   } else {
-      // 같은 네트워크(아이패드, 다른 PC)
-      baseURL = "http://192.168.0.15:8090";
-   }
-
-   console.log("현재 baseURL:", baseURL);
-
-   const instance = axios.create({
-      baseURL: baseURL,
+   var instance = axios.create({
+      baseURL: API_BASE,
       timeout: 5000,
-      withCredentials: true, // 세션/쿠키 필요하면 유지
+   })
+
+   token && instance.interceptors.request.use((config) => {
+
+      config.headers.Authorization = token;
+      return config;
    });
 
-   // 요청 인터셉터
-   if (token) {
-      instance.interceptors.request.use((config) => {
-         config.headers.Authorization = token;
-         return config;
-      });
-   }
-
-   // 응답 인터셉터
    instance.interceptors.response.use(
-      (response) => response,
-      (error) => {
+      response => response,
+      error => {
          const code = error.response?.status;
 
          if (code === 401 || code === 403) {
             alert("접근 권한이 없습니다.");
+
             store.set(accessTokenAtom, '');
             store.set(refreshTokenAtom, '');
+
             window.location.href = "/";
          }
 
@@ -102,4 +40,66 @@ export const myAxios = (token) => {
    );
 
    return instance;
+}
+
+export const themeObj = {
+   bgColor: "", 			// 바탕 배경색
+   searchBgColor: "", 		// 검색창 배경색
+   contentBgColor: "", 		// 본문 배경색(검색결과,결과없음,첫화면,검색서제스트)
+   pageBgColor: "", 		// 페이지 배경색
+   textColor: "", 			// 기본 글자색
+   queryTextColor: "", 		// 검색창 글자색
+   postcodeTextColor: "", 	// 우편번호 글자색
+   emphTextColor: "", 		// 강조 글자색
+   outlineColor: "" 		// 테두리
 };
+
+// export const myAxios = (token) => {
+//    let baseURL;
+
+//    // 현재 브라우저 hostname 가져오기
+//    const hostname = window.location.hostname;
+
+//    if (hostname === "localhost") {
+//       // 로컬 개발환경
+//       baseURL = "http://localhost:8090";
+//    } else {
+//       // 같은 네트워크(아이패드, 다른 PC)
+//       baseURL = "http://192.168.0.15:8090";
+//    }
+
+//    console.log("현재 baseURL:", baseURL);
+
+//    const instance = axios.create({
+//       baseURL: baseURL,
+//       timeout: 5000,
+//       withCredentials: true, // 세션/쿠키 필요하면 유지
+//    });
+
+//    // 요청 인터셉터
+//    if (token) {
+//       instance.interceptors.request.use((config) => {
+//          config.headers.Authorization = token;
+//          return config;
+//       });
+//    }
+
+//    // 응답 인터셉터
+//    instance.interceptors.response.use(
+//       (response) => response,
+//       (error) => {
+//          const code = error.response?.status;
+
+//          if (code === 401 || code === 403) {
+//             alert("접근 권한이 없습니다.");
+//             store.set(accessTokenAtom, '');
+//             store.set(refreshTokenAtom, '');
+//             window.location.href = "/";
+//          }
+
+//          return Promise.reject(error);
+//       }
+//    );
+
+//    return instance;
+// };
--- saladMan-frontend/src/firebaseconfig.js
+++ saladMan-frontend/src/firebaseconfig.js
@@ -1,51 +1,4 @@
-// import firebase from "firebase";
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-//   authDomain: "test-e93e5.firebaseapp.com",
-//   projectId: "test-e93e5",
-//   storageBucket: "test-e93e5.firebasestorage.app",
-//   messagingSenderId: "208770780932",
-//   appId: "1:208770780932:web:b7fc80ebade56c153e4a51",
-//   measurementId: "G-7BCY8BMVKE"
-// };
-
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-// const firebaseMessaging = firebaseApp.messaging();
-
-// export async function registerServiceWorker() {
-//   try {
-//     const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
-//     console.log("Service Worker 등록 성공:", registration);
-//   } catch (error) {
-//     console.log("Service Worker 등록 실패:", error);
-//   }    
-// }
-
-// export function firebaseReqPermission(setFcmToken, setAlarm) {
-//   firebaseMessaging
-//   .requestPermission()
-//   .then(() => {
-//     return firebaseMessaging.getToken({ vapidKey: import.meta.env.VITE_FIREBASE_VAPIKEY}); //등록 토큰 받기
-//   })
-//   .then(function (token) {
-//     console.log(token)
-//     setFcmToken(token);
-//   })
-//   .catch(function (error) {
-//     console.log("FCM Error : ", error);
-//   });
-
-//   firebaseMessaging.onMessage((payload) => {
-//     console.log(payload)
-//     setAlarm({num:+payload.data.num, title:payload.data.title, body:payload.data.body})
-//   });  
-// }
-
-//테스트 
-
-import firebase from "firebase/app";
-import "firebase/messaging";
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
@@ -58,16 +11,8 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseMessaging = firebaseApp.messaging();
 
-let firebaseMessaging;
-
-if (firebase.messaging.isSupported()) {
-  firebaseMessaging = firebaseApp.messaging();
-} else {
-  console.warn("이 브라우저에서는 Firebase Messaging이 지원되지 않습니다.");
-}
-
-// Service Worker 등록 함수
 export async function registerServiceWorker() {
   try {
     const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
@@ -77,34 +22,89 @@ export async function registerServiceWorker() {
   }    
 }
 
-// FCM 권한 요청 함수
 export function firebaseReqPermission(setFcmToken, setAlarm) {
-  if (!firebaseMessaging) {
-    console.warn("Messaging 미지원 브라우저");
-    return;
-  }
-
   firebaseMessaging
-    .requestPermission()
-    .then(() => {
-      return firebaseMessaging.getToken({
-        vapidKey: import.meta.env.VITE_FIREBASE_VAPIKEY
-      });
-    })
-    .then(function (token) {
-      console.log(token);
-      setFcmToken(token);
-    })
-    .catch(function (error) {
-      console.log("FCM Error : ", error);
-    });
+  .requestPermission()
+  .then(() => {
+    return firebaseMessaging.getToken({ vapidKey: import.meta.env.VITE_FIREBASE_VAPIKEY}); //등록 토큰 받기
+  })
+  .then(function (token) {
+    console.log(token)
+    setFcmToken(token);
+  })
+  .catch(function (error) {
+    console.log("FCM Error : ", error);
+  });
 
   firebaseMessaging.onMessage((payload) => {
-    console.log(payload);
-    setAlarm({
-      num: +payload.data.num,
-      title: payload.data.title,
-      body: payload.data.body
-    });
-  });
+    console.log(payload)
+    setAlarm({num:+payload.data.num, title:payload.data.title, body:payload.data.body})
+  });  
 }
+
+//테스트 
+
+// import firebase from "firebase/app";
+// import "firebase/messaging";
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+//   authDomain: "test-e93e5.firebaseapp.com",
+//   projectId: "test-e93e5",
+//   storageBucket: "test-e93e5.firebasestorage.app",
+//   messagingSenderId: "208770780932",
+//   appId: "1:208770780932:web:b7fc80ebade56c153e4a51",
+//   measurementId: "G-7BCY8BMVKE"
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// let firebaseMessaging;
+
+// if (firebase.messaging.isSupported()) {
+//   firebaseMessaging = firebaseApp.messaging();
+// } else {
+//   console.warn("이 브라우저에서는 Firebase Messaging이 지원되지 않습니다.");
+// }
+
+// // Service Worker 등록 함수
+// export async function registerServiceWorker() {
+//   try {
+//     const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+//     console.log("Service Worker 등록 성공:", registration);
+//   } catch (error) {
+//     console.log("Service Worker 등록 실패:", error);
+//   }    
+// }
+
+// // FCM 권한 요청 함수
+// export function firebaseReqPermission(setFcmToken, setAlarm) {
+//   if (!firebaseMessaging) {
+//     console.warn("Messaging 미지원 브라우저");
+//     return;
+//   }
+
+//   firebaseMessaging
+//     .requestPermission()
+//     .then(() => {
+//       return firebaseMessaging.getToken({
+//         vapidKey: import.meta.env.VITE_FIREBASE_VAPIKEY
+//       });
+//     })
+//     .then(function (token) {
+//       console.log(token);
+//       setFcmToken(token);
+//     })
+//     .catch(function (error) {
+//       console.log("FCM Error : ", error);
+//     });
+
+//   firebaseMessaging.onMessage((payload) => {
+//     console.log(payload);
+//     setAlarm({
+//       num: +payload.data.num,
+//       title: payload.data.title,
+//       body: payload.data.body
+//     });
+//   });
+// }
